3341e777aca94ba4f816fc13f4c1d5b450275093
liyue <602100452@qq.com>
2021-09-27 19:44:44 +0800

    提交

--- plugins/nuxt-swiper-plugin.js
+++ plugins/nuxt-swiper-plugin.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import css from 'swiper/dist/css/swiper.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 export default () => {
   Vue.use(VueAwesomeSwiper, {css})
 }
